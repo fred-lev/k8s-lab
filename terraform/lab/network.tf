0772d1dc8c65d7138c6a8cb3dc0af22534a892b6
fredflev <frederic.leveque@r3.com>
2021-06-29 00:00:13 +0800

    add port for linkerd

--- terraform/lab/network.tf
+++ terraform/lab/network.tf
@@ -35,7 +35,7 @@ resource "google_compute_firewall" "external" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "6443", "80", "8080"]
+    ports    = ["22", "6443", "80", "8080", "50750"]
   }
 
   source_ranges = ["${data.http.main.body}/32"]
